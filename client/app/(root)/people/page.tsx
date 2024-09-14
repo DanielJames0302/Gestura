1df6879624ac402d1593a9d870c39131b8e7d875
mman <minhmanuni@gmail.com>
2024-09-14 13:57:22 +0800

    fix vercel error

--- client/app/(root)/people/page.tsx
+++ client/app/(root)/people/page.tsx
@@ -4,7 +4,9 @@ import Loader from '@/components/Loader';
 import UserCard from '@/components/cards/UserCard';
 import { api } from '@/convex/_generated/api';
 import { useMutation, useQuery } from 'convex/react';
+import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
+import ErrorPage from 'next/error'
 
 const People = () => {
   const [loading, setLoading] = useState(true)
@@ -22,8 +24,11 @@ const People = () => {
     getAllUsers()
   }, [])
 
-  console.log(allUsers)
 
+  const router = useRouter()
+  if (!router.isFallback && !allUsers) {
+      return <ErrorPage statusCode={404} />
+  }
   return loading ? <Loader /> : (
     <div className='flex flex-col gap-4 py-6'>
       {allUsers && allUsers?.map((user: any) => (
